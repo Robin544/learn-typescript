8d9bff8ade3bdc0acda2ce095601d79b71c463e9
Robin Singh <robinsinghkamboj@gmail.com>
2024-01-01 23:52:46 +0530

    Using a Getter For People Count Rendering

--- src/app.ts
+++ src/app.ts
@@ -123,6 +123,11 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement>{
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
     private project: Project;
 
+    get persons() {
+        if (this.project.people === 1) return '1 person';
+        return `${this.project.people} persons`;
+    }
+
     constructor(hostId: string, project: Project) {
         super('single-project', hostId, true, project.id);
         this.project = project;
@@ -136,7 +141,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
     renderContent(): void {
         const { title, description, people } = this.project;
         this.element.querySelector('h2')!.textContent = title;
-        this.element.querySelector('h3')!.textContent = people.toString();
+        this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
         this.element.querySelector('p')!.textContent = description;
     }
 
