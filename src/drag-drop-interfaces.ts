f974b9a868dcee4ebd1974ef99b27f1498d5cf79
Robin Singh <robinsinghkamboj@gmail.com>
2024-01-03 21:44:45 +0530

    Working with Namespaces

---
+++ src/drag-drop-interfaces.ts
@@ -0,0 +1,14 @@
+namespace App {
+    // Draggable interface
+    export interface Draggable {
+        dragStartHandler(event: DragEvent): void;
+        dragEndHandler(event: DragEvent): void;
+    }
+
+    // Drag Target interface
+    export interface DragTarget {
+        dragOverHandler(event: DragEvent): void;
+        dropHandler(event: DragEvent): void;
+        dragLeaveHandler(event: DragEvent): void;
+    }
+}
