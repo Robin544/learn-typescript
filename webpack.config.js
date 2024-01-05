c6e7396543eea5d09cef00bd3b5f247ddee6682d
Robin Singh <robinsinghkamboj@gmail.com>
2024-01-05 08:13:30 +0530

    Finishing the Setup & Adding webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/app.ts',
+    devServer: {
+        static: [
+            {
+                directory: path.join(__dirname),
+            },
+        ],
+    },
     output: {
         filename: 'bundle.js', // bundle[contentHash].js to create a unique hash for every build.
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/',
     },
     devtool: 'inline-source-map',
     module: {
         rules: [
             {
-                test: /\.ts$/,
+                test: /\.tsx?$/,
                 use: 'ts-loader',
-                exclude: '/node_modules'
+                exclude: '/node_modules/'
             }
         ]
     },
